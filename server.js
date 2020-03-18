a3885aa4935455eb73bebe595a2c06073eb65f37
fighter201 <lusche682@gmail.com>
2020-03-18 11:14:10 +0100

    lulalale

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var port = 3000;
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-server.listen(port, function () {
-    console.log('Webserver läuft und hört auf Port %d', port);
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-app.use(express.static(__dirname + "/public"));
+io.on('connection', function(socket){
+    console.log("nutzer connected");
+    socket.broadcast.emit('hi');
+
+   socket.on('disconnect',function(){
+        console.log('user disconnected');
+   })
+  
+    socket.on('chat message', function(msg){
+        console.log("message is: " + msg);
+        io.emit('chat message', msg);
+    })
+
+})
+
+http.listen(3000, function(){
+    console.log('listening on port 3000');
+});
