12e19b472cfbe8e27fe20e694e951f00dc9f77ec
YNSKLG <37942412+YNSKLG@users.noreply.github.com>
2020-03-18 19:34:33 +0100

    Create head.inc.php

---
+++ inc/head.inc.php
@@ -0,0 +1,9 @@
+<?php session_start();?>
+
+<link rel="stylesheet" href="css/default.css">
+
+<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+
+<link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,700" rel="stylesheet">
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
