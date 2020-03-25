683165c38dbaec1b2a9fe76af6983d116ad142c5
Yannis Klügl <yannisktrash@gmail.com>
2020-03-25 22:15:27 +0100

    Login frontend

---
+++ inc/header-empty.inc.php
@@ -0,0 +1,6 @@
+<div class="header-container-empty">
+	<div class="headerlogoholder">
+		<img alt="logo" src="img/logo256.png">
+	</div>	
+	<a href="index.php">Lysin</a>
+</div>
--- login.php
+++ login.php
@@ -1,13 +1,23 @@
-<html>
+<html style="overflow: hidden">
 	<head>
 		<title>Lysin | Login</title>
 		<?php include_once 'inc/head.inc.php';?>
 	</head>
 	<body>
-		<?php include_once 'inc/header.inc.php';?>
+		<?php include_once 'inc/header-empty.inc.php';?>
 		<div class="page-container">	
 			<div class="content-wrap">
-				<p>eggs</p>
+				<div class="login-container">
+					<form>
+						<p style="font-weight: bold; font-size: 24px">Login</p>
+						<input type="text" name="name" class="input" placeholder="Benutzername" required>
+						<br><br>
+						<input type="password" name="pwd" class="input" placeholder="Passwort" required>
+						<br><br>
+						<button type="submit" name="submit" class="btn">Einloggen</button>
+					</form>
+					<button class="btn btn-register">Registrieren</button>
+				</div>
 			</div>
 			<?php include_once 'inc/footer.inc.php';?>
 		</div>	
